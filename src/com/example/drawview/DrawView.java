4ad88e0db9d7b2227fc10ee91b22570541391489
dalvik <sky.xctc@163.com>
2012-11-19 21:37:10 +0800

    drawview

---
+++ src/com/example/drawview/DrawView.java
@@ -0,0 +1,24 @@
+package com.example.drawview;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class DrawView extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_draw_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.layout_draw_view, menu);
+        return true;
+    }
+
+    
+}
