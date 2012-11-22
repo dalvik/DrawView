11c1e116eb3894c036a72ff7506756b1748cb5c2
dalvik <sky.xctc@163.com>
2012-11-22 14:55:14 +0800

    add scal by metrics

--- src/com/example/drawview/BackGroundVeiw.java
+++ src/com/example/drawview/BackGroundVeiw.java
@@ -5,30 +5,17 @@ import java.util.List;
 
 import android.R.color;
 import android.content.Context;
-import android.content.res.AssetManager;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.GestureDetector.OnGestureListener;
-import android.view.MotionEvent;
-import android.view.ScaleGestureDetector;
-import android.view.View;
+import android.widget.ImageView;
 
-public class BackGroundVeiw extends View implements OnGestureListener, ScaleGestureDetector.OnScaleGestureListener {
+public class BackGroundVeiw extends ImageView {
 
 	private Context context;
-	
-	private int rectWidth = 50;
-	
-	private int rectHeight = 50;
 	
 	private Paint paint = null;
 	
@@ -40,59 +27,9 @@ public class BackGroundVeiw extends View implements OnGestureListener, ScaleGest
 	
 	private boolean DEBUG = true;
 	
-	private boolean isScaling = false;
-	
-	private Paint MyVewPaint = new Paint();
-	
 	private Paint viewPathPaint = new Paint();
 	
-	private int oper = 0;
-	
-	private boolean addFlag = true;
-	
 	private Bitmap bg = null;//Bitmap.createBitmap(1000, 800, Config.RGB_565);
-	
-	private GestureDetector gestureDetector = null;
-	
-	private ScaleGestureDetector scaleGestureDetector = null;
-	
-	private int left = 0;
-	
-	private int top = 0;
-	
-	private int right = 0;
-	
-	private int bottom = 0;
-
-	private int screenWidth = 0;
-	
-	private int screenHeight = 0;
-	
-	private int displayWidth = 0;
-	
-	private int displayHeight = 0;
-	
-	int lastx = 0;
-	
-	int lasty = 0;
-	
-	private int lastLeft;
-
-	private int lastTop;
-
-	private int lastRight;
-
-	private int lastBottom;
-	
-	private int isloading = 0;
-	
-	private static int MAX = 0;
-	
-    private int dx1 = 45;
-    
-    private int dy1 = 60;
-	
-	private Rect rect = null;
 	
 	public BackGroundVeiw(Context context) {
 		super(context);
@@ -115,59 +52,17 @@ public class BackGroundVeiw extends View implements OnGestureListener, ScaleGest
 
 	private void initGesture() {
 		bg = BitmapFactory.decodeFile("/mnt/sdcard/IPED/Image/20120929150149.jpg");
-		System.out.println(bg.getWidth());
 		paint = new Paint();
 		paint.setColor(color.white);
-		MyVewPaint.setColor(Color.RED);
 		viewPathPaint.setColor(Color.BLUE);
 		viewPathPaint.setStrokeWidth(2);
 		viewPathPaint.setAntiAlias(true);
-		gestureDetector = new GestureDetector(this);
-		scaleGestureDetector = new ScaleGestureDetector(context,this);
-		setFocusable(true);
-		setLongClickable(true);
-		gestureDetector.setIsLongpressEnabled(true);
-		
-		right = bg.getWidth();
-		bottom = bg.getHeight();
-		MAX = right + 10 * dy1;
-		rect = new Rect(left, top, right, bottom);
-	}
-	
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		int counts = event.getPointerCount();
-		if(scaleGestureDetector.onTouchEvent(event) && counts>=2) {
-			return true;
-		}else if(gestureDetector.onTouchEvent(event)) {
-			return true;
-		}else {
-			/*if(oper == Constants.TOUCH) {
-				if(addFlag) {
-					addFlag = false;
-					MyView myView = new MyView(MyVewPaint, new Rect((int)event.getX() -  rectWidth/2, (int)event.getY() - rectHeight/2 , (int)event.getX() +  rectWidth/2,  (int)event.getY() + rectHeight/2));
-					int length = pointList.size();
-					if(length>0) {
-						MyView myViewTemp = pointList.get(length-1);
-						ViewPath viewPath = new ViewPath(myViewTemp.rect.centerX(), myViewTemp.rect.centerY(), myView.rect.centerX(), myView.rect.centerY(), viewPathPaint);
-						pathList.add(viewPath);
-					}
-					pointList.add(myView);
-					invalidate();
-					addFlag = true;
-				}
-			}*/
-		}
-		if(DEBUG) {
-			Log.d(TAG, "### touch x = " + event.getX() + " y = " + event.getY() + " oper = "+ oper);
-		}
-		return super.onTouchEvent(event);
 	}
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		canvas.drawBitmap(bg, null, new Rect(left , top, right, bottom), null);
+		//canvas.drawBitmap(bg, 0,0, null);
 		int length = pointList.size();
 		for(int i=0; i<length; i++) {
 			MyView myView = pointList.get(i);
@@ -180,213 +75,20 @@ public class BackGroundVeiw extends View implements OnGestureListener, ScaleGest
 			canvas.drawLine(viewPath.startX, viewPath.startY, viewPath.stopX, viewPath.stopY, viewPath.paint);
 		}
 	}
-
-	@Override
-	public boolean onDown(MotionEvent event) {
-		//oper = Constants.TOUCH;
-		lastx = (int)event.getRawX();
-		lasty = (int) event.getRawY();
-		if(DEBUG) {
-			Log.d(TAG, "### onDown x = " + event.getX() + " y = " + event.getY());
-		}
-		return true;
-	}
-
-	@Override
-	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-			float velocityY) {
-		oper = Constants.FILING;
-		if(DEBUG) {
-			Log.d(TAG, "### onFling x = " + e1.getX() + " y = " + e1.getY());
-		}
-		return true;
-	}
-
-	@Override
-	public void onLongPress(MotionEvent e) {
-		oper = Constants.LONGPRESS;
-		if(DEBUG) {
-			Log.d(TAG, "### onLongPress x = " + e.getX() + " y = " + e.getY());
-		}
-	}
-
-	@Override
-	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-			float distanceY) {
-		oper = Constants.SCROLL;
-		if(DEBUG) {
-			Log.d(TAG, "### onScroll x = " + e1.getX() + " y = " + e1.getY());
-		}
-		if(displayWidth > screenWidth) {
-			//Log.i(TAG, "scroll =" + tempBitmap.getWidth() + "  " + tempBitmap.getHeight() + " left=" + left + " top=" + top + " reight=" + right + " bottom=" + bottom);
-			int dx;
-			dx = (int) e2.getRawX() - lastx;
-			left = left + dx;
-			right = right + dx;
-			
-			if(left>=0) {
-				left = left - dx;
-				right = right - dx;
-			}
-
-			if(right<=screenWidth) {
-				right = right -dx;
-				left = left - dx;
-			}
-		}
-		if(displayHeight > screenHeight) {
-			int dy;
-			dy = (int) e2.getRawY() - lasty;
-			top = top + dy;
-			bottom = bottom + dy;
-			if(top>=0) {
-				top = top -dy;
-				bottom = bottom - dy;
-			}
- 
-			if(bottom<=screenHeight) {
-				bottom = bottom - dy;
-				top = top - dy;
-			}
-		}
-		this.invalidate();
-		lastx = (int) e2.getRawX();
-		lasty = (int) e2.getRawY(); 
-		return false;
-	}
-
-	@Override
-	public void onShowPress(MotionEvent e) {
-		
-	}
-
-	@Override
-	public boolean onSingleTapUp(MotionEvent e) {
-		if(DEBUG) {
-			Log.d(TAG, "### onSingleTapUp x = " + e.getX() + " y = " + e.getY());
-		}
-		oper = Constants.TOUCH;
-		return true;
-	}
-
-	private float lastFactor;
-    
-	private float currentFactor;
-    
-    
-	@Override
-	public boolean onScale(ScaleGestureDetector decDetector) {
-		if(DEBUG) {
-			Log.d(TAG, "### onScale");
-		}
-		currentFactor = decDetector.getScaleFactor();
-		float delta1 = currentFactor - lastFactor;
-        lastFactor = currentFactor;
-        Log.d(TAG, "delta1=" + delta1);
-       	 if (delta1 >= 0.02 && !onProcessing()) {
-       		 left = left - dx1;
-       		 top = top - dy1;
-       		 right = right + dx1;
-       		 bottom = bottom + dy1;
-       		 int max = right - left;
-       		 Log.d(TAG, "max=" + max);
-   		     if(max<=MAX && max>0) {
-   		 		this.invalidate();
-   		 		return false;
-   		 	 } else {
-   		 		 left = left + dx1;
-          		 top = top + dy1;
-          		 right = right - dx1;
-          		 bottom = bottom - dy1;
-          		 this.invalidate();
-   		 		 return false;
-   		 	 }
-	      } else if(delta1<=-0.021 && !onProcessing()) {
-	    	  if(top<0 && bottom<screenHeight) {
-		    	  top = top + dy1;
-		    	  bottom = bottom + dy1;
-	    	  } else if(top>0 && bottom>screenHeight) {
-	    		  top = top - dy1;
-		    	  bottom = bottom - dy1;
-	    	  }
-	    	  if(left>0 && right>screenWidth) {
-		    	  right -= (2*dx1);
-		    	  top += dy1;
-		    	  bottom -= dy1;
-	    	  } else  if(left < 0 && right < screenWidth) {
-	    		  left += (2*dx1);
-		    	  top += dy1;
-		    	  bottom -= dy1;
-	    	  }else {
-	    		  left += dx1;
-		    	  right -= dx1;
-		    	  top += dy1;
-		    	  bottom -= dy1;
-	    	  }
-	    	  int r_l = right - left;
-	    	  if(r_l <= 280) {
-	    		  left -= dx1;
-    	    	  top -= dy1;
-    	    	  right += dx1;
-    	    	  bottom += dy1;
-    	     }
-	     }
-       	this.invalidate();
-		return false;
-	}
-
-	@Override
-	public boolean onScaleBegin(ScaleGestureDetector detector) {
-		isScaling = true;
-		oper = Constants.SCALING;
-		lastFactor = detector.getScaleFactor();
-    	currentFactor = detector.getScaleFactor();
-    	dx1 = 45;
-    	dy1 = 60;
-    	lastLeft = left;
- 		lastTop = top;
- 		lastRight = right;
- 		lastBottom = bottom;
-		if(DEBUG) {
-			Log.d(TAG, "### onScaleBegin");
-		}
-		return true;
-	}
-
-	@Override
-	public void onScaleEnd(ScaleGestureDetector detector) {
-		isScaling = false;
-		oper = Constants.TOUCH;
-		isloading = 0;
-		int hopeWidth = right-left;
-		int homeHeight = bottom-top;
-		Bitmap temp = BitmapUtil.adaptive(screenWidth, screenHeight, bg);
-		if(temp != null) {
-			bg.recycle();
-			bg = null;
-			System.gc();
-			bg = temp;
-		}
-		if(DEBUG) {
-			Log.d(TAG, "### onScaleEnd");
-		}
-	}
-
 	
-	public void initXY(int width, int height) {
-		screenWidth = width;
-		screenHeight = height;
-		isloading = 0;
-		if(displayWidth <= screenWidth) {
-			left = (screenWidth - displayWidth)/2;
-			right = displayWidth + left;
-		}
-		if(DEBUG){
-			Log.d(TAG, "### initXY left=" + left + "  right=" + right);
-		}
+	public Bitmap getBitmap() {
+		return bg;
 	}
 	
-	private boolean onProcessing() {
-		return isloading == 1;
+	public void addPoint(MyView myView) {
+		//MyView myView = new MyView(MyVewPaint, new Rect((int)event.getX() -  rectWidth/2, (int)event.getY() - rectHeight/2 , (int)event.getX() +  rectWidth/2,  (int)event.getY() + rectHeight/2));
+		int length = pointList.size();
+		if(length>0) {
+			MyView myViewTemp = pointList.get(length-1);
+			ViewPath viewPath = new ViewPath(myViewTemp.rect.centerX(), myViewTemp.rect.centerY(), myView.rect.centerX(), myView.rect.centerY(), viewPathPaint);
+			pathList.add(viewPath);
+		}
+		pointList.add(myView);
+		invalidate();
 	}
 }
--- src/com/example/drawview/DrawView.java
+++ src/com/example/drawview/DrawView.java
@@ -1,38 +1,211 @@
 package com.example.drawview;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.WindowManager;
-import android.support.v4.app.NavUtils;
+import android.util.FloatMath;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
 
-public class DrawView extends Activity {
+public class DrawView extends Activity implements OnTouchListener {
 
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+    DisplayMetrics dm;
+    BackGroundVeiw imgView;
+    Bitmap bitmap;
 
-	private BackGroundVeiw bgVeiw =  null;
-	
+    float minScaleR;// 最小缩放比例
+    static final float MAX_SCALE = 4f;// 最大缩放比例
+
+    static final int NONE = 0;// 初始状态
+    static final int DRAG = 1;// 拖动
+    static final int ZOOM = 2;// 缩放
+    int mode = NONE;
+
+    PointF prev = new PointF();
+    PointF mid = new PointF();
+    float dist = 1f;
+
+    
+    private Paint MyVewPaint = new Paint();
+    private String TAG = "Touch";
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_draw_view);
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        bgVeiw = (BackGroundVeiw) findViewById(R.id.blank_view);
-        bgVeiw.initXY(displayMetrics.widthPixels, displayMetrics.heightPixels);
-       // bgVeiw.setOnClickListener(this);
+
+		MyVewPaint.setColor(Color.RED);
+        imgView = (BackGroundVeiw) findViewById(R.id.imag);// 获取控件
+        //bitmap = BitmapFactory.decodeResource(getResources(), this.getIntent().getExtras().getInt("IMG"));// 获取图片资源
+        //bitmap = BitmapFactory.decodeFile("/mnt/sdcard/IPED/Image/20120929150149.jpg");
+        bitmap = imgView.getBitmap();
+        imgView.setImageBitmap(bitmap);// 填充控件
         
+        imgView.setOnTouchListener(this);// 设置触屏监听
+        dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);// 获取分辨率
+        minZoom();
+        center();
+        imgView.setImageMatrix(matrix);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.layout_draw_view, menu);
+    public boolean onTouchEvent(MotionEvent event) {
+    	Log.d(TAG, " onclock  ");
+    	return super.onTouchEvent(event);
+    }
+    /**
+     * 触屏监听
+     */
+    public boolean onTouch(View v, MotionEvent event) {
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+        // 主点按下
+        case MotionEvent.ACTION_DOWN:
+            savedMatrix.set(matrix);
+            prev.set(event.getX(), event.getY());
+            mode = DRAG;
+            Log.d(TAG, "### ACTION_DOWN");
+            break;
+        // 副点按下
+        case MotionEvent.ACTION_POINTER_DOWN:
+            dist = spacing(event);
+            // 如果连续两点距离大于10，则判定为多点模式
+            if (spacing(event) > 10f) {
+                savedMatrix.set(matrix);
+                midPoint(mid, event);
+                mode = ZOOM;
+            }
+            Log.d(TAG, "### ACTION_POINTER_DOWN");
+            break;
+        case MotionEvent.ACTION_UP:
+        case MotionEvent.ACTION_POINTER_UP:
+        	if(mode != DRAG) {
+            	MyView myView = new MyView(MyVewPaint, new Rect((int)event.getX() -  50/2, (int)event.getY() - 50/2 , (int)event.getX() +  50/2,  (int)event.getY() + 50/2));
+            	imgView.addPoint(myView);
+            	Log.d(TAG, "### add point");
+            }
+            mode = NONE;
+            Log.d(TAG, "### ACTION_UP");
+            break;
+        case MotionEvent.ACTION_MOVE:
+            if (mode == DRAG) {
+                matrix.set(savedMatrix);
+                matrix.postTranslate(event.getX() - prev.x, event.getY() - prev.y);
+            } else if (mode == ZOOM) {
+                float newDist = spacing(event);
+                if (newDist > 10f) {
+                    matrix.set(savedMatrix);
+                    float tScale = newDist / dist;
+                    matrix.postScale(tScale, tScale, mid.x, mid.y);
+                }
+            }
+            Log.d(TAG, "### ACTION_MOVE");
+            break;
+        }
+        imgView.setImageMatrix(matrix);
+        CheckView();
         return true;
     }
 
-    
-}
+    /**
+     * 限制最大最小缩放比例，自动居中
+     */
+    private void CheckView() {
+        float p[] = new float[9];
+        matrix.getValues(p);
+        if (mode == ZOOM) {
+            if (p[0] < minScaleR) {
+                matrix.setScale(minScaleR, minScaleR);
+            }
+            if (p[0] > MAX_SCALE) {
+                matrix.set(savedMatrix);
+            }
+        }
+        center();
+    }
 
+    /**
+     * 最小缩放比例，最大为100%
+     */
+    private void minZoom() {
+        minScaleR = Math.min((float) dm.widthPixels / (float) bitmap.getWidth(), (float) dm.heightPixels / (float) bitmap.getHeight());
+        if (minScaleR < 1.0) {
+            matrix.postScale(minScaleR, minScaleR);
+        }
+    }
+
+    private void center() {
+        center(true, true);
+    }
+
+    /**
+     * 横向、纵向居中
+     */
+    protected void center(boolean horizontal, boolean vertical) {
+
+        Matrix m = new Matrix();
+        m.set(matrix);
+        RectF rect = new RectF(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        m.mapRect(rect);
+
+        float height = rect.height();
+        float width = rect.width();
+
+        float deltaX = 0, deltaY = 0;
+
+        if (vertical) {
+            // 图片小于屏幕大小，则居中显示。大于屏幕，上方留空则往上移，下方留空则往下移
+            int screenHeight = dm.heightPixels;
+            if (height < screenHeight) {
+                deltaY = (screenHeight - height) / 2 - rect.top;
+            } else if (rect.top > 0) {
+                deltaY = -rect.top;
+            } else if (rect.bottom < screenHeight) {
+                deltaY = imgView.getHeight() - rect.bottom;
+            }
+        }
+
+        if (horizontal) {
+            int screenWidth = dm.widthPixels;
+            if (width < screenWidth) {
+                deltaX = (screenWidth - width) / 2 - rect.left;
+            } else if (rect.left > 0) {
+                deltaX = -rect.left;
+            } else if (rect.right < screenWidth) {
+                deltaX = screenWidth - rect.right;
+            }
+        }
+        matrix.postTranslate(deltaX, deltaY);
+    }
+
+    /**
+     * 两点的距离
+     */
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return FloatMath.sqrt(x * x + y * y);
+    }
+
+    /**
+     * 两点的中点
+     */
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
+    }
+}
